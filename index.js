31807c41ff5210042fd2ca94f8d5235a972a7f3a
dhananjayjakhadi <31828887+dhananjayjakhadi@users.noreply.github.com>
2019-06-20 21:05:53 -0400

    List out courses and id

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const app = express();
 
-// const courses = [
-//     {id: 1, course: 'newCourse1'},
-//     {id: 2, course: 'newCourse1'},
-//     {id: 3, course: 'newCourse1'}
-// ]
+const courses = [
+    {id: 1, course: 'newCourse1'},
+    {id: 2, course: 'newCourse1'},
+    {id: 3, course: 'newCourse1'}
+];
 app.get("/", (req, res) => {
     res.send('Welcome to App!!');
 })
 
 app.get("/api/courses", (req, res) => {
-  res.send([1,2,3]);  
+  res.send(courses);  
 })
 
 app.get("/api/courses/:id", (req, res) => {
-    res.send(req.params.id);  
-})
-
-app.get("/api/posts/:year/:month", (req, res) => {
-    res.send(req.query);  
+    const course = courses.find(c => c.id === parseInt(req.params.id)); 
+    if(!course) res.status(404).send('Course not found');
+    res.send(course);
 })
 
 const port =  process.env.PORT || 3000;
