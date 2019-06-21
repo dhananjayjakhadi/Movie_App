9b0a34ef10e74904bb11d581a8fea3bc8876de26
dhananjayjakhadi <31828887+dhananjayjakhadi@users.noreply.github.com>
2019-06-20 22:19:33 -0400

    Delete request for a course

--- index.js
+++ index.js
@@ -6,22 +6,24 @@ app.use(express.json());
 
 const courses = [
     {id: 1, name: 'newCourse1'},
-    {id: 2, name: 'newCourse1'},
-    {id: 3, name: 'newCourse1'}
+    {id: 2, name: 'newCourse2'},
+    {id: 3, name: 'newCourse3'}
 ];
+
+
 app.get("/", (req, res) => {
     res.send('Welcome to App!!');
-})
+});
 
 app.get("/api/courses", (req, res) => {
   res.send(courses);  
-})
+});
 
 app.get("/api/courses/:id", (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id)); 
     if(!course) res.status(404).send('Course not found');
     res.send(course);
-})
+});
 
 app.post("/api/courses", (req, res) => {
     const {error} = validateCourse(req.body);
@@ -47,24 +49,26 @@ app.put("/api/courses/:id", (req, res) => {
         res.status(400).send(error.details[0].message);
         return;
     }
-
     course.name = req.body.name;
     res.send(course);
 });
 
+app.delete("/api/courses/:id", (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id)); 
+    if(!course) return req.status(404).send('course not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.send(course);
+});
+
+   
 function validateCourse(course){
     const schema = {
         name: Joi.string().min(3).required()
     };
     return Joi.validate(course, schema);
 }
-
-
-
-
-
-
-
 
 const port =  process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is Listening on ${port}`));
